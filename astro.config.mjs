ab268a707132120b05bdd8c80dc1efdf35f93912
Jonathan <j_moreno@uncg.edu>
2024-06-20 20:24:42 -0400

    Success

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://JM-2001.github.io",
-  base: "Portfolio",
+  base: "main",
 });
