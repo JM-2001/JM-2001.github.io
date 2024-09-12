01eab652aa9dd2309a098945d036408e4e2797b9
Jonathan <j_moreno@uncg.edu>
2024-09-12 12:02:19 -0400

    CName update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://JM-2001.github.io",
-  base: "main",
+  site: "JM2001.tech",
+  //site: "https://JM-2001.github.io",
+  //base: "main",
 });
