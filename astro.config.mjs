5f612742f5a857aaa36fdee850a431d623b25a32
Jonathan <j_moreno@uncg.edu>
2024-09-12 20:47:53 -0400

    SSL fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "JM2001.tech",
+  site: "https://www.JM2001.tech",
   //site: "https://JM-2001.github.io",
   //base: "main",
 });
